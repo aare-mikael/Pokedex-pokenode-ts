925037f1c8a65f95efdbb9c204b954423a5bb1a9
Mikael Aare <aaremikael@gmail.com>
2023-10-10 17:42:36 +0200

    Created fetchPokemon.tsx

--- api/fetchPokemon.tsx
+++ api/fetchPokemon.tsx
@@ -1,47 +1,12 @@
-// import { PokemonData } from '@/interfaces/pokemonData';
-// import { fetchPokemonByName } from './fetchPokemonByName';
-// import { client } from './api';
-// import { gql } from '@apollo/client';
+import { api } from './api';
 
-// const PokemonQuery = (lim: number) => gql`
-//   query {
-//     pokemons(offset: 0, limit: ${lim}) {
-//       results {
-//         name
-//         image
-//       }
-//     }
-//   }
-// `;
-
-// async function getPokemonNames(lim: number) {
-//   try {
-//     const { data } = await client.query({ query: PokemonQuery(lim) });
-//     const names: String[] = [];
-//     for (let index = 0; index < data.pokemons.results.length; index++) {
-//       const element = data.pokemons.results[index].name;
-//       names.push(element);
-//     }
-//     return names;
-//   } catch (error) {
-//     console.error('Error fetching X Pokemon', error);
-//     throw error;
-//   }
-// }
-
-// export async function fetchPokemon(lim: number) {
-//   const names = await getPokemonNames(lim);
-
-//   const pokemons: PokemonData[] = [];
-//   for (let index = 0; index < names.length; index++) {
-//     let name = names[index];
-//     try {
-//       let fetchedPokemon = await fetchPokemonByName(name);
-//       pokemons.push(fetchedPokemon);
-//     } catch (error) {
-//       console.error('Error populating pokemons', error);
-//       throw error;
-//     }
-//   }
-//   return pokemons;
-// }
+export async function fetchPokemon() {
+  try {
+    const count = (await api.pokemon.listPokemons()).count;
+    // First number is offset, second is limit
+    return (await api.pokemon.listPokemons(0, count)).results;
+  } catch (error) {
+    console.error('Error in fetchPokemon.tsx, error fetching Pokemon', error);
+    throw error;
+  }
+}
