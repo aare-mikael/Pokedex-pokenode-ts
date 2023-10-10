e674d80ef42fa2f3e9468f5455e20b84f57042bc
Mikael Aare <aaremikael@gmail.com>
2023-10-10 17:43:38 +0200

    Created fetchPokemonByName.tsx

--- api/fetchPokemonByName.tsx
+++ api/fetchPokemonByName.tsx
@@ -1,53 +1,10 @@
-// import { PokemonData } from '../interfaces/pokemonData';
-// import { client } from './api';
-// import { gql } from '@apollo/client';
+import { api } from './api';
 
-// const queryPokemonByName = gql`
-//   query getOnePokemon($name: String!) {
-//     pokemon(name: $name) {
-//       abilities {
-//         ability {
-//           name
-//         }
-//       }
-//       base_experience
-//       game_indices {
-//         version {
-//           name
-//         }
-//       }
-//       height
-//       id
-//       name
-//       sprites {
-//         front_default
-//       }
-//       stats {
-//         stat {
-//           name
-//         }
-//         base_stat
-//       }
-//       types {
-//         slot
-//         type {
-//           name
-//         }
-//       }
-//       weight
-//     }
-//   }
-// `;
-
-// export async function fetchPokemonByName(name: String): Promise<PokemonData> {
-//   try {
-//     const { data } = await client.query({
-//       query: queryPokemonByName,
-//       variables: { name },
-//     });
-//     return data.pokemon;
-//   } catch (error) {
-//     console.error('Error fetching Pokemon by name:', error);
-//     throw error;
-//   }
-// }
+export async function fetchPokemonByName(name: string) {
+  try {
+    return await api.pokemon.getPokemonByName(name);
+  } catch (error) {
+    console.error('Error fetching Pokemon', error);
+    throw error;
+  }
+}
