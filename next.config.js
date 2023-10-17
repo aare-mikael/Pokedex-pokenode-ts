0002d48c7a13fe2ce1ff67aa76b5296703281b5e
Mikael Aare <aaremikael@gmail.com>
2023-10-17 12:49:26 +0200

    Added domains to get pictures from

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = nextConfig;
+module.exports = {
+    images: {
+        domains: ['localhost', 'raw.githubusercontent.com'],
+    },
+    };
+    nextConfig;
