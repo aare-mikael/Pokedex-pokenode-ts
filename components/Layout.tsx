4b5f938075810186c680a7425938f5544c3d345d
Mikael Aare <aaremikael@gmail.com>
2023-10-18 06:38:00 +0200

    Added type and ability functionality

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,13 +4,38 @@ import Image from 'next/image';
 import SearchBar from './Search/SearchBar';
 import Link from 'next/link';
 import PokemonTile from './PokemonTile';
+import TypeTile from './TypeTile';
+import AbilityTile from './AbilityTile';
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [selectedPokemon, setSelectedPokemon] = useState<string | null>(null);
+  const [searchResults, setSearchResults] = useState<
+    Array<{
+      item: string;
+      filter: 'pokemon' | 'types' | 'abilities';
+      entering: boolean;
+    }>
+  >([]);
+
+  const addResult = (item: string, filter: 'pokemon' | 'types') => {
+    const newResult = { item, filter, entering: true };
+    setSearchResults((prevResults) => [newResult, ...prevResults]);
+
+    setTimeout(() => {
+      setSearchResults((prevResults) =>
+        prevResults.map((result) => {
+          if (result === newResult) {
+            return { ...result, entering: false };
+          } else {
+            return result;
+          }
+        })
+      );
+    }, 1000);
+  };
 
   return (
     <div className={styles.container}>
@@ -21,13 +46,23 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           </Link>
         </div>
         <SearchBar
-          onSearch={(query) => {
-            setSelectedPokemon(query);
+          onSearch={(query, filter) => {
+            addResult(query, filter);
           }}
         />
       </header>
       <main className={styles.main}>
-        {selectedPokemon && <PokemonTile name={selectedPokemon} />}
+        {searchResults.map((result, i) => (
+          <div key={i} className={`${result.entering ? styles.entering : ''}`}>
+            {result.filter === 'pokemon' ? (
+              <PokemonTile name={result.item} />
+            ) : result.filter === 'types' ? (
+              <TypeTile name={result.item} />
+            ) : (
+              <AbilityTile name={result.item} />
+            )}
+          </div>
+        ))}
         {children}
       </main>
       <footer className={styles.footer}>
